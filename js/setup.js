549aaac5da17b6e13c68d14760ce4c38bcc1ea7e
Ivan Klimukhin <ivankmn@yandex.ru>
2018-12-07 18:13:31 +0300

    4-1

--- js/setup.js
+++ js/setup.js
@@ -49,6 +49,14 @@ var eyesColor = [
   'green'
 ];
 
+var fireballColor = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 var getFeatureValue = function (featuresArr) {
   var randomValue = Math.floor(Math.random() * featuresArr.length);
   return featuresArr[randomValue];
@@ -137,3 +145,61 @@ openIcon.addEventListener('keydown', function (evt) {
     openPopup();
   }
 });
+
+
+var warlock = userDialog.querySelector('.setup-wizard');
+var coat = warlock.querySelector('.wizard-coat');
+var eyes = warlock.querySelector('.wizard-eyes');
+var fireball = userDialog.querySelector('.setup-fireball-wrap');
+var inputCoatColor = userDialog.querySelector('input[name="coat-color"]');
+var inputEyesColor = userDialog.querySelector('input[name="eyes-color"]');
+var inputFireColor = userDialog.querySelector('input[name="fireball-color"]');
+console.log(inputFireColor);
+
+var getNextValue = function (featuresArr, feature, styleColor) {
+  var currColor = feature.style[styleColor];
+  var nextValue = 1;
+  featuresArr.forEach(function(item, i) {
+    if (item === currColor) {
+      nextValue = i+1;
+    }
+  });
+  if (nextValue >= featuresArr.length) {
+    nextValue = 0;
+  }
+  return featuresArr[nextValue];
+};
+
+
+var changeCoatColor = function (evt) {
+  if (evt.target.classList.contains('wizard-coat')) {
+    coat.style.fill = getNextValue(coatColor, coat, 'fill');
+    inputCoatColor.value = coat.style.fill;
+  }
+};
+
+var changeEyesColor = function (evt) {
+  if (evt.target.classList.contains('wizard-eyes')) {
+    eyes.style.fill = getNextValue(eyesColor, eyes, 'fill');
+    inputEyesColor.value = eyes.style.fill;
+  }
+};
+
+var changeFireColor = function () {
+  fireball.style.background = getNextValue(fireballColor, fireball, 'background');
+  inputFireColor.value = fireball.style.background;
+};
+
+var warlockClickHandler = function (wizard) {
+  wizard.addEventListener('click', changeCoatColor);
+  wizard.addEventListener('click', changeEyesColor);
+};
+
+var fireballClickHandler = function (fire) {
+  fire.addEventListener('click', changeFireColor);
+  console.log(inputFireColor);
+};
+
+warlockClickHandler(warlock);
+
+fireballClickHandler(fireball);
