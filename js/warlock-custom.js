e5107de9000f7bee734d35679548c363599a84bc
Ivan Klimukhin <ivankmn@yandex.ru>
2019-01-23 10:18:43 +0300

    правки

--- js/warlock-custom.js
+++ js/warlock-custom.js
@@ -26,13 +26,7 @@
   };
 
   var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
+    return left - right;
   };
 
   var updateWarlocks = function (data) {
